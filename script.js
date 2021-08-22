a74c8084d90f1d9f5bf32736cb56628c97a0b854
quahho <quahho@gmail.com>
2021-08-22 16:37:53 +0800

    Added UI

--- script.js
+++ script.js
@@ -1,91 +1,217 @@
+const resetLink = document.querySelector('#reset');
+
+resetLink.addEventListener('click', () => {
+    window.location.reload();
+});
+
+const buttons = document.querySelectorAll('.choice');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay());
+    });
+});
+
+function toggleEnlarge() {
+    const buttons = document.querySelectorAll('.choice');
+    buttons.forEach((button) => {
+        button.classList.toggle('enlarge');
+    });
+}
+
+function highlightPlayerChoice(selection) {
+    let buttonId = `#${selection}`;
+    const button = document.querySelector(buttonId);
+    button.style.backgroundColor = 'darkgrey'; 
+}
+
+function highlightComputerChoice(selection) {
+    let buttonId = `#c${selection}`;
+    const button = document.querySelector(buttonId);
+    button.style.backgroundColor = 'darkgrey'; 
+}
+
+function clearHighlight() {
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+        button.style.backgroundColor = 'beige';
+    });
+}
+
+function getPlayerCurrentPoint() {
+    const currentScore = document.querySelector('#ppoint');
+    return +(currentScore.textContent);
+}
+
+function getComputerCurrentPoint() {
+    const currentScore = document.querySelector('#cpoint');
+    return +(currentScore.textContent);
+}
+
+function setPoint(winner) {
+    if (winner === 'player') {
+        const currentScore = document.querySelector('#ppoint');
+        newScore = +(currentScore.textContent) + 1;
+        currentScore.textContent = newScore;
+    }
+    else if (winner === 'computer') {
+        const currentScore = document.querySelector('#cpoint');
+        newScore = +(currentScore.textContent) + 1;
+        currentScore.textContent = newScore;
+    }
+}
+
+function setCurrentTie() {
+    let playerScore = getPlayerCurrentPoint();
+    let computerScore = getComputerCurrentPoint();
+    let status;
+
+    if (playerScore > computerScore) {
+        status = 'You are currently ahead.';
+    }
+    else if (playerScore < computerScore) {
+        status = 'You are trailing behind.';
+    }
+    else {
+        status = 'All square right now.';
+    }
+
+    const button = document.querySelector('#progress-button');
+    button.id = 'message';
+    button.textContent = status;
+}
+
+function setCurrentMessage() {
+    const label = document.querySelector('#result');
+    label.textContent = 'The battle continues...';
+}
+
+function setResult(result) {
+    const label = document.querySelector('#result');
+    label.textContent = result;
+}
+
+function setOverallResult(endResult) {
+    const button = document.querySelector('#progress-button');
+    button.id = 'overall-result';
+    button.textContent = endResult;
+}
+
+function refreshRound() {
+    if (!newRound) {
+        clearHighlight();
+        toggleEnlarge();
+        setCurrentMessage();
+        setCurrentTie();
+        newRound = true;
+    }
+}
+
+function setNextRoundButton() {
+    const button = document.querySelector('#message');
+    button.id = 'progress-button';
+    button.textContent = 'Next Round';
+    button.addEventListener('click', refreshRound);
+}
+
+function clearNextRoundButton() {
+    const button = document.querySelector('#progress-button');
+    button.removeEventListener('click', refreshRound);
+}
+
+function checkEndGame() {
+    let playerScore = getPlayerCurrentPoint();
+    let computerScore = getComputerCurrentPoint();
+    let endResult;
+
+    if (playerScore === 5) {
+        clearNextRoundButton();
+        endResult = "GAME OVER : You are the winner.";
+        setOverallResult(endResult);
+        gameEnded = true;
+    }
+    else if (computerScore === 5) {
+        clearNextRoundButton();
+        endResult = "GAME OVER : You are the loser.";
+        setOverallResult(endResult);
+        gameEnded = true;
+    }
+    else {
+        return;
+    }
+}
+
 function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"]
     var index = Math.floor(Math.random() * choices.length);
     return choices[index].toUpperCase();
 }
 
+let newRound = true;
+
 function playRound(playerSelection, computerSelection) {
+    if (!newRound) {
+        return;
+    }
+
+    newRound = false;
+    toggleEnlarge();
+
+    highlightPlayerChoice(playerSelection);
+    highlightComputerChoice(computerSelection);
+
     let result;
+    let winner;
+    let playerChoice = playerSelection.slice(1);
 
-    if (playerSelection === "ROCK") {
+    if (playerChoice.toUpperCase() === "ROCK") {
         switch (computerSelection) {
             case "PAPER":
-                result = "You Lose! Paper beats Rock";
+                result = "You Lose! Paper beats Rock.";
+                winner = "computer";
                 break;
             case "SCISSORS":
-                result = "You Win! Rock beats Scissors";
+                result = "You Win! Rock beats Scissors.";
+                winner = "player";
                 break;
             default:
-                result = "It's a Draw! Rock with Rock";
+                result = "It's a Draw! Rock with Rock.";
                 break;
         }
     }
-    else if (playerSelection === "PAPER") {
+    else if (playerChoice.toUpperCase() === "PAPER") {
         switch (computerSelection) {
             case "SCISSORS":
-                result = "You Lose! Scissors beats Paper";
+                result = "You Lose! Scissors beats Paper.";
+                winner = "computer";
                 break;
             case "ROCK":
-                result = "You Win! Paper beats Rock";
+                result = "You Win! Paper beats Rock.";
+                winner = "player";
                 break;
             default:
-                result = "It's a Draw! Paper with Paper";
+                result = "It's a Draw! Paper with Paper.";
                 break;
         }
     }
-    else {
+    else if (playerChoice.toUpperCase() === "SCISSORS") {
         switch (computerSelection) {
             case "ROCK":
-                result = "You Lose! Rock beats Scissors";
+                result = "You Lose! Rock beats Scissors.";
+                winner = "computer";
                 break;
             case "PAPER":
-                result = "You Win! Scissors beats Paper";
+                result = "You Win! Scissors beats Paper.";
+                winner = "player";
                 break;
             default:
-                result = "It's a Draw! Scissors with Scissors";
+                result = "It's a Draw! Scissors with Scissors.";
                 break;
         }
     }
 
-    return result;
+    setPoint(winner);
+    setResult(result);
+    setNextRoundButton();
+    checkEndGame();
 }
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let x = 0; x < 5; x++) {
-        let userInput = prompt("Rock, Paper or Scissors?");
-        let playerSelection = userInput.toUpperCase();
-
-        while (playerSelection !== "ROCK" && playerSelection !== "PAPER" && playerSelection !== "SCISSORS") {
-            userInput = prompt("Invalid option. Try again. Rock, Paper or Scissors?");
-            playerSelection = userInput.toUpperCase();
-        }
-
-        const computerSelection = computerPlay();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-        if (result.includes("Win")) {
-            playerScore++;
-        }
-        else if (result.includes("Lose")) {
-            computerScore++;
-        }
-    }
-
-    switch (true) {
-        case playerScore > computerScore:
-            console.log(`You are the winner. Overall score ${playerScore}:${computerScore}`);
-            break;
-        case playerScore < computerScore:
-            console.log(`You are the loser. Overall score ${playerScore}:${computerScore}`);
-            break;
-        default:
-            console.log(`It's a stalemate. Overall score ${playerScore}:${computerScore}`);
-            break;
-    }
-}
-
-game();
